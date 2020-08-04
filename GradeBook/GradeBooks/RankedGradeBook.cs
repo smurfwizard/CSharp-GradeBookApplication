b219d3776c3d040f9d708edb780b0dd1583a3570
smurfwizard <wizard@sunflower.com>
2020-08-04 12:52:54 -0500

    mode1

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,10 @@
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook: BaseGradeBook
+    {
+        public RankedGradeBook(string name):base(name)
+        {
+            Type = Enums.GradeBookType.Ranked;
+        }
+    }
+}
---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,10 @@
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+         public StandardGradeBook(string name) : base(name)
+         {
+            Type = Enums.GradeBookType.Standard;
+         }
+    }
+}
