6b57c65ae348ca5ce8204e96119762c73fea1287
smurfwizard <wizard@sunflower.com>
2020-08-05 11:53:30 -0500

    Update StartingUserInterface.cs

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -78,6 +78,7 @@ namespace GradeBook.UserInterfaces
             if (gradeBook == null)
                 return;
 
+///
             GradeBookUserInterface.CommandLoop(gradeBook);
         }
 
@@ -86,7 +87,7 @@ namespace GradeBook.UserInterfaces
             Console.WriteLine();
             Console.WriteLine("GradeBook accepts the following commands:");
             Console.WriteLine();
-            Console.WriteLine("Create 'Name' 'Type' 'Weighted' - Creates a new gradebook where 'Name' is the name of the gradebook, and 'Type' is what type of grading it should use, and 'Weighted' is whether grades should be weighted (true or false).");
+            Console.WriteLine("Create 'Name' 'Type' 'Weighted' - Creates a new gradebook where 'Name' is the name of the gradebook, 'Type' is what type of grading it should use, and 'Weighted' is whether or not grades should be weighted (true or false).");
             Console.WriteLine();
             Console.WriteLine("Load 'Name' - Loads the gradebook with the provided 'Name'.");
             Console.WriteLine();
